06498b1f49e1f44cddb4121cdcd02bc664b122ce
Chris Finn <chris@finn.com>
2019-09-30 19:39:47 -0400

    Added navigate mock instead of wrapping component in navigator

--- __tests__/HomeScreen.test.tsx
+++ __tests__/HomeScreen.test.tsx
@@ -1,20 +1,15 @@
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from '../src/screens/HomeScreen';
 
-function renderWithNavigation() {
-  const AppNavigator = createStackNavigator({
-    Home: HomeScreen,
-  });
+var module = require('react-navigation-hooks');
+module.useNavigation = jest.fn().mockImplementation(() => {
+  return {
+    navigate: jest.fn(),
+  };
+});
 
-  const App = createAppContainer(AppNavigator);
-
-  return <App />;
-}
 it('renders correctly', () => {
-  const Home = renderWithNavigation();
-  renderer.create(Home);
+  renderer.create(<HomeScreen />);
 });
