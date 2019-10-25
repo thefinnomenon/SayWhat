789a0dbd0c1687907aa77effb5f873b2091048ac
Chris Finn <chris@finn.com>
2019-10-25 19:58:18 -0400

    Added Sports & Games wordlist. Added Everything wordlist.

--- assets/wordlists/everything.ts
+++ assets/wordlists/everything.ts
@@ -1,10 +1,19 @@
+import plants_and_animals from './animals_and_plants';
+import around_the_house from './around_the_house';
+import food_and_drink from './food_and_drink';
+import geography from './geography';
+import history from './history';
+import media from './media';
+import sports_and_games from './sports_and_games';
+
 export const Everything = [
-  'DVD Player',
-  'VCR',
-  'Walkman',
-  'CD Player',
-  'BluRay Player',
-  'Surround Sound',
+  ...plants_and_animals,
+  ...around_the_house,
+  ...food_and_drink,
+  ...geography,
+  ...history,
+  ...media,
+  ...sports_and_games,
 ];
 
 export default Everything;
